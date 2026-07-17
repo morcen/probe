4a413672788ade8b62976cadb64b76d175e3a96e
Morris Jencen Chavez <hello@morcen.com>
2026-07-18 04:04:28 +0800

    add tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,11 +2,14 @@
 
 namespace Morcen\Probe\Tests;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Morcen\Probe\ProbeServiceProvider;
 use Orchestra\Testbench\TestCase as BaseTestCase;
 
 class TestCase extends BaseTestCase
 {
+    use RefreshDatabase;
+
     protected function getPackageProviders($app): array
     {
         return [
